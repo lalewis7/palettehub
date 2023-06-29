fae70b0a533b53c3aab9b8f85711912b233f0002
lalewis7 <l.arthur.lewis7@gmail.com>
2023-06-29 12:45:11 -0400

    Created Maven project

---
+++ rest_api/src/main/java/net/palettehub/api/App.java
@@ -0,0 +1,13 @@
+package net.palettehub.api;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
