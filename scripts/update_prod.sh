759c10dee52e0931c2705c6ab833ce9a02140662
lalewis7 <l.arthur.lewis7@gmail.com>
2023-09-25 07:04:28 -0400

    add shutdown line to save resources

--- scripts/update_prod.sh
+++ scripts/update_prod.sh
@@ -9,5 +9,8 @@ docker image prune -f
 # pull changes from github
 git pull
 
+# shutdown to save resources
+docker compose -f docker-compose.yaml -f docker-compose.prod.yaml down
+
 # build and deploy
 docker compose -f docker-compose.yaml -f docker-compose.prod.yaml up --build -d
